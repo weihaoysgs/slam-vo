86bd2c6a7037c518f977a52d1a5121f95d842f91
weihao <weihaoysgs@gmail.com>
2023-02-27 18:37:41 +0800

    Test config class success

--- test/config_test.cpp
+++ test/config_test.cpp
@@ -14,5 +14,9 @@ int main(int argc, char *argv[]) {
   slam_vo::InitGlog("config_test");
   slam_vo::Config::SetParameterFile(FLAGS_yaml_config_file);
   LOG(INFO) << slam_vo::Config::GetYamlParamByKey<std::string>("dataset_dir");
+  LOG(INFO) << "fx: " << slam_vo::Config::GetYamlParamByKey<double>("camera.fx");
+  LOG(INFO) << "fy: " << slam_vo::Config::GetYamlParamByKey<double>("camera.fy");
+  LOG(INFO) << "cx: " << slam_vo::Config::GetYamlParamByKey<double>("camera.cx");
+  LOG(INFO) << "cy: " << slam_vo::Config::GetYamlParamByKey<double>("camera.cy");
   return 0;
 }
