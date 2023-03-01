46f5951f4a7e677305ccdf25369f2618ac3067dd
weihao <weihaoysgs@gmail.com>
2023-03-01 13:29:14 +0800

    Create Map class

---
+++ include/Map.hpp
@@ -0,0 +1,17 @@
+#ifndef SLAMVO_MAP_HPP_
+#define SLAMVO_MAP_HPP_
+
+#include "Common.hpp"
+
+namespace slam_vo {
+class Map {
+ public:
+  Map();
+  ~Map();
+
+ private:
+};
+
+}  // namespace slam_vo
+
+#endif  // SLAMVO_MAP_HPP_
