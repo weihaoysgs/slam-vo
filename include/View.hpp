177bba3d4ba3eb77487e949cd52dbf555645301b
weihao <weihaoysgs@gmail.com>
2023-03-01 13:39:43 +0800

    Cerate View class  files

---
+++ include/View.hpp
@@ -0,0 +1,13 @@
+#ifndef SLAM_VO_VIEW_HPP_
+#define SLAM_VO_VIEW_HPP_
+
+namespace slam_vo {
+class View {
+ public:
+  View();
+  ~View();
+};
+
+}  // namespace slam_vo
+
+#endif  // SLAM_VO_VIEW_HPP_
